7bc2946ffee01d2a633697fbb1537147ebff2089
Craig Brooks <cwbrooks2003@yahoo.com>
2018-06-08 16:48:04 -0400

    add files

---
+++ public/assets/js/app.js
@@ -0,0 +1,23 @@
+//Sign in jquery
+
+
+
+//global variables
+
+
+
+//  Start on click.
+$("#siginButton").on("click", function storeData() {
+//info form inputEmail2
+
+});
+
+//  Done on click.
+$("#siginButton").on("click", );// calls method to send user to library.html
+
+
+
+
+
+
+
