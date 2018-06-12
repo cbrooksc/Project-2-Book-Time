4172b6e3be2169b31fa16973abb9afc63cd9388e
Craig Brooks <cwbrooks2003@yahoo.com>
2018-06-12 13:16:24 -0400

    change the folder structure to MVC

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,30 +4,30 @@
 
 
 
-$(".gridImg-1").on("click", function(event){
-  event.preventDefault();
+// $(".gridImg-1").on("click", function(event){
+//   event.preventDefault();
 
 
-$.get("/api/main/id:1", function(data) {
+// $.get("/api/main/id:1", function(data) {
 
-var books = $("<div>");
+// var books = $("<div>");
 
-books.addClass("bookData");
-
-
-
-books.append("<p>" + data.Title + "</p>");
-books.append("<p>" + data.AUTHOR + "</p>");
-books.append("<p>" + data.Genres + "</p>");
-books.append("<p>" + data.PUBLISHED_YEAR + "</p>");
-books.append("<p>" + data.SUMMARY + "</p>");
-books.append("<p>" + data.URL_LINK + "</p>");
+// books.addClass("bookData");
 
 
 
-$(".grid-alpha").append(books);
+// books.append("<p>" + data.Title + "</p>");
+// books.append("<p>" + data.AUTHOR + "</p>");
+// books.append("<p>" + data.Genres + "</p>");
+// books.append("<p>" + data.PUBLISHED_YEAR + "</p>");
+// books.append("<p>" + data.SUMMARY + "</p>");
+// books.append("<p>" + data.URL_LINK + "</p>");
 
 
 
-});
-});
+// $(".grid-alpha").append(books);
+
+
+
+// });
+// });
